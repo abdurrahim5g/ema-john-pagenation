ac1d63b40939058d399041269dd86ec8ae29acf1
f-rahim <freelancerrahim26@gmail.com>
2023-06-10 13:19:56 +0600

    Added pagenation facelity

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useLoaderData } from "react-router-dom";
+import { Link } from "react-router-dom";
 import {
   addToDb,
   deleteShoppingCart,
@@ -10,8 +10,26 @@ import Product from "../Product/Product";
 import "./Shop.css";
 
 const Shop = () => {
-  const products = useLoaderData();
+  // console.log(count);
   const [cart, setCart] = useState([]);
+
+  const [limit, setLimit] = useState(10);
+  const [count, setCount] = useState(0);
+  const [totalPage, setTotalPage] = useState(Math.ceil(count / limit));
+  const [currentPage, setCurrentPage] = useState(0);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `http://localhost:5000/products?currentPage=${currentPage}&limit=${limit}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.products);
+        setTotalPage(Math.ceil(count / limit));
+        setCount(data.count);
+      });
+  }, [count, currentPage, limit]);
 
   const clearCart = () => {
     setCart([]);
@@ -53,14 +71,46 @@ const Shop = () => {
 
   return (
     <div className="shop-container">
-      <div className="products-container">
-        {products.map((product) => (
-          <Product
-            key={product._id}
-            product={product}
-            handleAddToCart={handleAddToCart}
-          ></Product>
-        ))}
+      <div className="">
+        <div className="products-container">
+          {products?.map((product) => (
+            <Product
+              key={product._id}
+              product={product}
+              handleAddToCart={handleAddToCart}
+            ></Product>
+          ))}
+        </div>
+        <div className="pagenation" style={{ width: "80%", margin: "0 auto" }}>
+          <h3>Pagenation</h3>
+          <div className="pagenations-buttons">
+            {[...Array(totalPage).keys()].map((p) => (
+              <button
+                key={p}
+                className={currentPage === p ? "selected" : ""}
+                onClick={() => setCurrentPage(p)}
+              >
+                {p + 1}
+              </button>
+            ))}
+            <div className="per-page-products">
+              <span>Products per page: </span>
+              <select
+                name="perPage"
+                id="perPage"
+                onChange={(e) => {
+                  setLimit(parseInt(e.target.value));
+                  setCurrentPage(0);
+                }}
+                defaultValue={limit}
+              >
+                <option value={10}>10</option>
+                <option value={20}>20</option>
+                <option value={50}>50</option>
+              </select>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="cart-container">
         <Cart clearCart={clearCart} cart={cart}>
